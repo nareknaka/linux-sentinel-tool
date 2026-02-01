2e6bad3a9e7b9a6138a3d747ebcb0911be5046fe
Narek Grigoryan <grigoryan.narek1993@gmail.com>
2026-02-01 19:00:00 +0400

    feat: add functions for CPU, RAM and Disk metrics

--- modules/monitor.sh
+++ modules/monitor.sh
@@ -21,3 +21,20 @@ TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 # Log the execution start time to the file
 echo "--- Check started at: $TIMESTAMP ---" >> "$LOG_FILE"
+
+
+
+# 1. Functions to get system data
+cpu_usage() {
+    top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}'
+
+}
+
+memory_usage() {
+    free | awk '/Mem/ {printf "%.2f", $3/$2 * 100}'
+
+}
+
+disk_usage() {
+    df / | awk 'NR==2 {print $5}' | sed 's/%//'
+}
