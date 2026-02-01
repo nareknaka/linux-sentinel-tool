8f9b520e46fefd52c6a60929cf2d815d115b9b9f
Narek Grigoryan <grigoryan.narek1993@gmail.com>
2026-02-01 20:44:38 +0400

    fix: update version to 1.1.0, fix header and adjust timezone

--- modules/monitor.sh
+++ modules/monitor.sh
@@ -3,10 +3,10 @@
 
 
 # Script Name: monitor.sh
-# Description: Monitors system resurces (Ram, Disk) and logs warnings.
+# Description: Monitors system resources (CPU, Ram, Disk) and logs warnings.
 # Author:      Narek <https://github.com/nareknaka>
 # Date:        2026-01-31
-# Version:     1.0.0
+# Version:     1.1.0
 
 # Usage:       ./modules/monitor.sh
 # Part of:     Linux Sentinel Tool
@@ -17,7 +17,7 @@
 LOG_FILE="../logs/system_monitor.log"
 
 # Generate a current timestamp
-TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+TIMESTAMP=$(TZ="Asia/Yerevan" date "+%Y-%m-%d %H:%M:%S")
 
 # Log the execution start time to the file
 echo "--- Check started at: $TIMESTAMP ---" >> "$LOG_FILE"
