c5eb71f67c2c3e18d1686a76448af354ed40d382
Narek Grigoryan <grigoryan.narek1993@gmail.com>
2026-02-05 21:31:11 +0400

    docs: add script header and initial package list

---
+++ modules/packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+# Script Name: packages.sh
+# Description: Checks and installs required dependencies (bc, curl, ufw, etc.)
+# Author:      Narek <https://github.com/nareknaka>
+# Date:        2026-02-05
+# Version:     1.0.0
+
+# Usage:       ./modules/packages.sh
+# Part of:     Linux Sentinel Tool
+
+
+
